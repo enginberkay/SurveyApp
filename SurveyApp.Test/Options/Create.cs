47f19381adf26cd6cd2c4cf6991bddff1b492640
berkay.engin <berkayengin0@gmail.com>
2019-03-17 13:24:48 +0300

    Create test project and sample test case

---
+++ SurveyApp.Test/Options/Create.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveyApp.Models;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SurveyApp.Test.Options
+{
+    public class Create
+    {
+        [Fact]
+        public async Task CreateOption()
+        {
+            using (var db = new PostgresContext(Utilities.DB.TestDbContextOptions()))
+            {
+                Option option = new Option();
+                option.Name = "Unit Test Option";
+                option.Description = "First Test for create metod";
+                var createModel = new Pages.Options.CreateModel(db);
+                createModel.Option = option;
+                var result = await createModel.OnPostAsync();
+                Assert.IsType<RedirectToPageResult>(result);
+            }
+        }
+
+        [Fact]
+        public async Task CreateNullOption()
+        {
+            using (var db = new PostgresContext(Utilities.DB.TestDbContextOptions()))
+            {
+                Option option = new Option();
+                var createModel = new Pages.Options.CreateModel(db);
+                createModel.Option = option;
+                var result = await createModel.OnPostAsync();
+                Assert.IsType<RedirectToPageResult>(result);
+            }
+        }
+    }
+}
---
+++ SurveyApp.Test/Utilities/DB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SurveyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SurveyApp.Test.Utilities
+{
+    class DB
+    {
+        #region snippet1
+        public static DbContextOptions<PostgresContext> TestDbContextOptions()
+        {
+            // Create a new service provider to create a new in-memory database.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance using an in-memory database and 
+            // IServiceProvider that the context should resolve all of its 
+            // services from.
+            var builder = new DbContextOptionsBuilder<PostgresContext>()
+                .UseInMemoryDatabase("InMemoryDb")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+        #endregion
+    }
+}
